63690650d88d28c60134f3e9f161513285039879
Rubick <eithanllanosb@gmail.com>
2024-12-11 14:16:37 -0500

    Add CRUD and endpoints to EmployeeRestController

--- apiCrudDemo/src/main/java/com/springCourse/apiCrudDemo/rest/EmployeeRestController.java
+++ apiCrudDemo/src/main/java/com/springCourse/apiCrudDemo/rest/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.springCourse.apiCrudDemo.rest;
 
 import com.springCourse.apiCrudDemo.entity.Employee;
 import com.springCourse.apiCrudDemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +43,30 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+        return theEmployee;
+    }
+
+    // add mapping POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        // Also just in case they pass an Id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+
     }
 }
