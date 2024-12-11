53c1c2aa7fdc1b124c565c4cbb6ed48c6ab6c845
Rubick <eithanllanosb@gmail.com>
2024-12-11 14:17:08 -0500

    Add setters to Employee.java attributes

--- apiCrudDemo/src/main/java/com/springCourse/apiCrudDemo/entity/Employee.java
+++ apiCrudDemo/src/main/java/com/springCourse/apiCrudDemo/entity/Employee.java
@@ -39,17 +39,34 @@ public class Employee {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getEmail() {
         return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
     // define toString
 
